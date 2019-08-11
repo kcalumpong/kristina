e8851e88a8dd6052ce11edbeda938725d0735dee
Kristina Calumpong <kcalumpong@gmail.com>
2019-08-11 16:46:30 -0700

    changed background to white

---
+++ javascript/portfolio.js
@@ -0,0 +1,12 @@
+var kristina = {
+  location: "losAngeles",
+  skills:
+    ["html",
+      "css",
+      "javascript",
+      "jQuery",
+      "bootstrap",
+      "wordpress",
+      "squarespace",
+    ],
+}
